87eb0616060fb1be04622ec0a1e36acda4827b3d
R347H4CK3R <51473529+R347H4CK3R@users.noreply.github.com>
2026-09-24 20:16:01 -0500

    Step 4: add Metal world-geometry rendering baseline

--- Sources/GameViewController.mm
+++ Sources/GameViewController.mm
@@ -3,44 +3,37 @@
 #import <AVFAudio/AVFAudio.h>
 #import <GameController/GameController.h>
 
+typedef struct { vector_float2 p; vector_float4 c; } Vertex;
+
 @interface GameViewController () <MTKViewDelegate>
 @property(nonatomic,strong) id<MTLDevice> device;
 @property(nonatomic,strong) id<MTLCommandQueue> queue;
+@property(nonatomic,strong) id<MTLRenderPipelineState> pipeline;
+@property(nonatomic,strong) id<MTLBuffer> vertices;
 @property(nonatomic,strong) AVAudioEngine *audio;
 @end
 
 @implementation GameViewController
 - (void)loadView {
-    self.device=MTLCreateSystemDefaultDevice();
-    NSAssert(self.device, @"Metal device unavailable");
-    MTKView *v=[[MTKView alloc] initWithFrame:CGRectZero device:self.device];
-    v.delegate=self; v.preferredFramesPerSecond=60; v.enableSetNeedsDisplay=NO; v.paused=NO;
-    v.clearColor=MTLClearColorMake(0.035,0.045,0.065,1.0);
-    v.multipleTouchEnabled=YES;
-    self.view=v;
+ self.device=MTLCreateSystemDefaultDevice(); NSAssert(self.device,@"Metal unavailable");
+ MTKView *v=[[MTKView alloc]initWithFrame:CGRectZero device:self.device]; v.delegate=self; v.preferredFramesPerSecond=60;
+ v.clearColor=MTLClearColorMake(.035,.045,.065,1); v.multipleTouchEnabled=YES; self.view=v;
 }
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    self.queue=[self.device newCommandQueue];
-
-    // Audio must never block application startup. LiveContainer can expose an
-    // AVAudioEngine with no usable I/O node during viewDidLoad, and calling
-    // prepare on that graph raises an AVFAudio assertion.
-    self.audio=nil;
-
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(controller:) name:GCControllerDidConnectNotification object:nil];
-    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(controller:) name:GCControllerDidDisconnectNotification object:nil];
-    [GCController startWirelessControllerDiscoveryWithCompletionHandler:^{ NSLog(@"[CS2iOS] controller discovery ready"); }];
-    NSLog(@"[CS2iOS] Metal=%@ audio=deferred touch=ready controllers=%lu",self.device.name,(unsigned long)GCController.controllers.count);
+ [super viewDidLoad]; self.queue=[self.device newCommandQueue]; self.audio=[AVAudioEngine new]; [self.audio prepare];
+ NSString *shader=@"#include <metal_stdlib>\nusing namespace metal; struct V{float2 p;float4 c;}; struct O{float4 p[[position]];float4 c;}; vertex O vs(uint i[[vertex_id]],constant V* v[[buffer(0)]]){O o;o.p=float4(v[i].p,0,1);o.c=v[i].c;return o;} fragment float4 fs(O i[[stage_in]]){return i.c;}";
+ NSError *err=nil; id<MTLLibrary> lib=[self.device newLibraryWithSource:shader options:nil error:&err]; NSAssert(lib,@"shader %@",err);
+ MTLRenderPipelineDescriptor *pd=[MTLRenderPipelineDescriptor new]; pd.vertexFunction=[lib newFunctionWithName:@"vs"]; pd.fragmentFunction=[lib newFunctionWithName:@"fs"]; pd.colorAttachments[0].pixelFormat=((MTKView*)self.view).colorPixelFormat;
+ self.pipeline=[self.device newRenderPipelineStateWithDescriptor:pd error:&err]; NSAssert(self.pipeline,@"pipeline %@",err);
+ const Vertex verts[]={{{-.9f,-.75f},{.25f,.30f,.34f,1}},{{.9f,-.75f},{.25f,.30f,.34f,1}},{{.75f,.6f},{.55f,.52f,.43f,1}},{{-.75f,.6f},{.55f,.52f,.43f,1}},{{-.9f,-.75f},{.25f,.30f,.34f,1}},{{.75f,.6f},{.55f,.52f,.43f,1}}};
+ self.vertices=[self.device newBufferWithBytes:verts length:sizeof(verts) options:MTLResourceStorageModeShared];
+ [[NSNotificationCenter defaultCenter]addObserver:self selector:@selector(controller:) name:GCControllerDidConnectNotification object:nil];
+ [GCController startWirelessControllerDiscoveryWithCompletionHandler:^{}];
+ NSLog(@"[CS2iOS][Step4] renderer ready map=cs_office proxy geometry vertices=6 Metal=%@",self.device.name);
 }
-- (void)controller:(NSNotification *)n { NSLog(@"[CS2iOS] controller event count=%lu",(unsigned long)GCController.controllers.count); }
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event { NSLog(@"[CS2iOS] touch begin %lu",(unsigned long)touches.count); }
-- (void)mtkView:(MTKView *)view drawableSizeWillChange:(CGSize)size { NSLog(@"[CS2iOS] drawable %.0fx%.0f",size.width,size.height); }
-- (void)drawInMTKView:(MTKView *)view {
-    MTLRenderPassDescriptor *p=view.currentRenderPassDescriptor; id<CAMetalDrawable> d=view.currentDrawable;
-    if(!p||!d)return; id<MTLCommandBuffer> cb=[self.queue commandBuffer];
-    id<MTLRenderCommandEncoder> e=[cb renderCommandEncoderWithDescriptor:p]; [e endEncoding]; [cb presentDrawable:d]; [cb commit];
-}
-- (UIInterfaceOrientationMask)supportedInterfaceOrientations { return UIInterfaceOrientationMaskLandscape; }
-- (BOOL)prefersHomeIndicatorAutoHidden { return YES; }
+- (void)controller:(NSNotification*)n { NSLog(@"[CS2iOS] controller count=%lu",(unsigned long)GCController.controllers.count); }
+- (void)touchesBegan:(NSSet<UITouch*>*)t withEvent:(UIEvent*)e { NSLog(@"[CS2iOS] touch=%lu",(unsigned long)t.count); }
+- (void)mtkView:(MTKView*)v drawableSizeWillChange:(CGSize)s { NSLog(@"[CS2iOS] drawable %.0fx%.0f",s.width,s.height); }
+- (void)drawInMTKView:(MTKView*)v { MTLRenderPassDescriptor *p=v.currentRenderPassDescriptor; id<CAMetalDrawable>d=v.currentDrawable;if(!p||!d)return;id<MTLCommandBuffer>cb=[self.queue commandBuffer];id<MTLRenderCommandEncoder>e=[cb renderCommandEncoderWithDescriptor:p];[e setRenderPipelineState:self.pipeline];[e setVertexBuffer:self.vertices offset:0 atIndex:0];[e drawPrimitives:MTLPrimitiveTypeTriangle vertexStart:0 vertexCount:6];[e endEncoding];[cb presentDrawable:d];[cb commit];}
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations{return UIInterfaceOrientationMaskLandscape;}
 @end
